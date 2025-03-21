ca2e2a34990a750d7007b988ef8f9411a2c2ee70
Óscar Rabadán Martínez <rabadanoscar@outlook.es>
2025-03-21 23:20:19 +0100

    Tailwind, componetizacion, types, consts

---
+++ src/consts/skills.ts
@@ -0,0 +1,96 @@
+import type { Skills } from '@/types/skills'
+
+export const SKILLS: Skills[] = [
+{
+    id: 'html5',
+    name: 'Html',
+},
+{
+    id: 'css3',
+    name: 'CSS',
+},
+{
+    id: 'sass',
+    name: 'Sass',
+},
+{
+    id: 'markdown',
+    name: 'Markdown',
+},
+{
+    id: 'javascript',
+    name: 'Javascript',
+},
+{
+    id: 'typescript',
+    name: 'Typescript',
+},
+{
+    id: 'jquery',
+    name: 'jQuery',
+},
+{
+    id: 'bootstrap',
+    name: 'Bootstrap',
+},
+{
+    id: 'tailwindcss',
+    name: 'Tailwind CSS',
+},
+{
+    id: 'nextjs',
+    name: 'NextJS',
+},
+{
+    id: 'astro',
+    name: 'Astro',
+},
+{
+    id: 'vite',
+    name: 'Vite',
+},
+{
+    id: 'webpack',
+    name: 'Webpack',
+},
+{
+    id: 'redux',
+    name: 'Redux',
+},
+{
+    id: 'github',
+    name: 'Github',
+},
+{
+    id: 'vercel',
+    name: 'Vercel',
+},
+{
+    id: 'netlify',
+    name: 'Netlify',
+},
+{
+    id: 'supabase',
+    name: 'Supabase',
+},
+{
+    id: 'firabase',
+    name: 'Firabase',
+},
+{
+    id: 'figma',
+    name: 'Figma',
+},
+{
+    id: 'photoshop',
+    name: 'Photoshop',
+},
+{
+    id: 'notion',
+    name: 'Notion',
+},
+{
+    id: 'trello',
+    name: 'Trello',
+}
+] as const;
