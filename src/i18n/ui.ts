62569f0377d9e976a2828b7044d1a904667ca35e
Óscar Rabadán Martínez <rabadanoscar@outlook.es>
2025-03-22 03:11:35 +0100

    Idiomas

---
+++ src/i18n/ui.ts
@@ -0,0 +1,75 @@
+export const languages = {
+    en: 'en',
+    fr: 'fr',
+    es: 'es',
+
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    en: {
+        'nav.home': 'Presentation',
+        'nav.work': 'Featured Work',
+        'nav.contact': 'Contact',
+
+        'hero.desc': "Frontend developer with over 3 years of experience, I like to keep up to date with the latest industry trends and continuously improve my knowledge.Focused on creating responsive and high- performance web applications with great attention to detail.",
+        'hero.job': "Frontend Developer",
+        'hero.link': 'Would you like to check some of my projects? Go for it!',
+
+        'skills.languages': 'Languages',
+        'skills.frameworks': 'Frameworks and Libraries',
+        'skills.clouds': 'Clouds and Providers',
+        'skills.tools': 'Tools',
+
+        'form.name': 'Name',
+        'form.email': 'Email',
+        'form.subject': 'Subject',
+        'form.message': 'Say something...',
+        'form.send': 'Send',
+
+    },
+    fr: {
+        'nav.home': 'Présentation',
+        'nav.work': 'Projets en vedette',
+        'nav.contact': 'Contact',
+
+        'hero.desc': "Développeur front-end avec plus de 3 ans d'expérience, j'aime me tenir au courant des dernières tendances du secteur et améliorer continuellement mes connaissances. Je me concentre sur la création d'applications web réactives et performantes, avec un grand souci du détail.",
+        'hero.job': "Développeur front-end",
+        'hero.link': "Envie de découvrir mes projets? N'hésitez pas!",
+
+        'skills.languages': 'Langages',
+        'skills.frameworks': 'Frameworks et librairies',
+        'skills.clouds': 'Clouds et Fournisseurs de services',
+        'skills.tools': 'Outils',
+
+        'form.name': 'Nom complet',
+        'form.email': 'Email',
+        'form.subject': 'Objet',
+        'form.message': 'Dites quelque chose...',
+        'form.send': 'Envoyer',
+    },
+    es: {
+        'nav.home': 'Presentación',
+        'nav.work': 'Proyectos destacados',
+        'nav.contact': 'Contacto',
+
+        'hero.desc': "Frontend Developer con más de 3 años de experiencia. Me gusta mantenerme al día con las últimas tendencias del sector y mejorar continuamente mis conocimientos. Me enfoco en crear aplicaciones web responsivas y de alto rendimiento, prestando especial atención al detalle.",
+        'hero.job': "Frontend Developer",
+        'hero.link': '¿Te gustaría ver algunos de mis proyectos?',
+
+        'skills.languages': 'Lenguajes',
+        'skills.frameworks': 'Frameworks y librerias',
+        'skills.clouds': 'Clouds y proveedores de servicio',
+        'skills.tools': 'Herramientas',
+
+        'form.name': 'Nombre completo',
+        'form.email': 'Correo electrónico',
+        'form.subject': 'Asunto',
+        'form.message': 'Escribe aqui tu mensaje...',
+        'form.send': 'Enviar',
+
+    },
+} as const;
+
+export const showDefaultLang = false;
