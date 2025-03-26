ccaa7cb257816fbca1218ccde9d8d8b94e5c1895
Óscar Rabadán Martínez <rabadanoscar@outlook.es>
2025-03-26 02:19:26 +0100

    colaboraciones destacadas

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,6 +11,7 @@ export const ui = {
     en: {
         'nav.home': 'Presentation',
         'nav.work': 'Featured Work',
+        'nav.collaborations': 'Collaborations',
         'nav.contact': 'Contact',
 
         'hero.desc': "Frontend developer with over 3 years of experience, I like to keep up to date with the latest industry trends and continuously improve my knowledge.Focused on creating responsive and high- performance web applications with great attention to detail.",
@@ -22,6 +23,10 @@ export const ui = {
         'skills.clouds': 'Clouds and Providers',
         'skills.tools': 'Tools',
 
+        'project.tubookbuddy.description': 'TuBookBuddy is a web platform designed for book lovers, where users can easily discover, organize, and share books. Developed with Next.js and styled with TailwindCSS, the app offers a fast and fluid experience.',
+        'collaborations.title': 'Featured Collaborations',
+
+        'form.title': "Don't hesitate to contact me!",
         'form.name': 'Name',
         'form.email': 'Email',
         'form.subject': 'Subject',
@@ -32,6 +37,7 @@ export const ui = {
     fr: {
         'nav.home': 'Présentation',
         'nav.work': 'Projets en vedette',
+        'nav.collaborations': 'Collaborations',
         'nav.contact': 'Contact',
 
         'hero.desc': "Développeur front-end avec plus de 3 ans d'expérience, j'aime me tenir au courant des dernières tendances du secteur et améliorer continuellement mes connaissances. Je me concentre sur la création d'applications web réactives et performantes, avec un grand souci du détail.",
@@ -43,6 +49,10 @@ export const ui = {
         'skills.clouds': 'Clouds et Fournisseurs de services',
         'skills.tools': 'Outils',
 
+        'project.tubookbuddy.description': "TuBookBuddy est une plateforme Web conçue pour les amateurs de livres, où les utilisateurs peuvent facilement découvrir, organiser et partager des livres. Propulsée par Next.js et conçue avec TailwindCSS, l'application offre une expérience rapide et fluide.",
+        'collaborations.title': 'Collaborations en vedette',
+
+        'form.title': "N'hésitez pas à me contacter !",
         'form.name': 'Nom complet',
         'form.email': 'Email',
         'form.subject': 'Objet',
@@ -52,17 +62,22 @@ export const ui = {
     es: {
         'nav.home': 'Presentación',
         'nav.work': 'Proyectos destacados',
+        'nav.collaborations': 'Colaboraciones',
         'nav.contact': 'Contacto',
 
         'hero.desc': "Frontend Developer con más de 3 años de experiencia. Me gusta mantenerme al día con las últimas tendencias del sector y mejorar continuamente mis conocimientos. Me enfoco en crear aplicaciones web responsivas y de alto rendimiento, prestando especial atención al detalle.",
         'hero.job': "Frontend Developer",
         'hero.link': '¿Te gustaría ver algunos de mis proyectos?',
 
+        'project.tubookbuddy.description': 'TuBookBuddy es una plataforma web diseñada para amantes de la lectura, donde los usuarios pueden descubrir, organizar y compartir libros de manera sencilla. Desarrollada con Next.js y estilizada con TailwindCSS, la aplicación ofrece una experiencia rápida y fluida.',
+        'collaborations.title': 'Colaboraciones destacadas',
+
         'skills.languages': 'Lenguajes',
         'skills.frameworks': 'Frameworks y librerias',
         'skills.clouds': 'Clouds y proveedores de servicio',
         'skills.tools': 'Herramientas',
 
+        'form.title': '¡No dudes en contactarme!',
         'form.name': 'Nombre completo',
         'form.email': 'Correo electrónico',
         'form.subject': 'Asunto',
