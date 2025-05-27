9c9d44bb0e43ff105c65f9f551c93e981f3018bc
Oscrandio <rabadanoscar@outlook.es>
2025-05-27 16:01:39 +0200

    styles ffixed

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,99 +1,105 @@
 export const languages = {
-    en: 'en',
-    fr: 'fr',
-    es: 'es',
-
+  en: "en",
+  fr: "fr",
+  es: "es",
 };
 
-export const defaultLang = 'es';
+export const defaultLang = "es";
 
 export const ui = {
-    en: {
-        'nav.home': 'Presentation',
-        'nav.work': 'Featured Work',
-        'nav.collaborations': 'Collaborations',
-        'nav.contact': 'Contact',
+  en: {
+    "tab.title": "Óscar Rabadán's Portfolio",
 
-        'hero.desc': "Frontend developer with over 3 years of experience, I like to keep up to date with the latest industry trends and continuously improve my knowledge. Focused on creating responsive and high- performance web applications with great attention to detail.",
-        'hero.job': "Frontend Developer",
-        'hero.link': 'Would you like to check some of my projects? Go for it!',
+    "nav.home": "Presentation",
+    "nav.work": "Featured Work",
+    "nav.collaborations": "Collaborations",
+    "nav.contact": "Contact",
 
-        'skills.languages': 'Languages',
-        'skills.frameworks': 'Frameworks and Libraries',
-        'skills.clouds': 'Clouds and Providers',
-        'skills.tools': 'Tools',
+    "hero.desc":
+      "Frontend developer with over 3 years of experience, I like to keep up to date with the latest industry trends and continuously improve my knowledge. Focused on creating responsive and high- performance web applications with great attention to detail.",
+    "hero.job": "Frontend Developer",
+    "hero.link": "Would you like to check some of my projects? Go for it!",
 
-        'project.tubookbuddy.description': 'TuBookBuddy is a web platform designed for book lovers, where users can easily discover, organize, and share books. Developed with Next.js and styled with TailwindCSS, the app offers a fast and fluid experience.',
-        'collaborations.title': 'Featured Collaborations',
+    "skills.languages": "Languages",
+    "skills.frameworks": "Frameworks and Libraries",
+    "skills.clouds": "Clouds and Providers",
+    "skills.tools": "Tools",
 
-        'form.title': "Don't hesitate to contact me!",
-        'form.name': 'Name',
-        'form.email': 'Email',
-        'form.subject': 'Subject',
-        'form.message': 'Say something...',
-        'form.send': 'Send',
+    "project.tubookbuddy.description":
+      "TuBookBuddy is a web platform designed for book lovers, where users can easily discover, organize, and share books. Developed with Next.js and styled with TailwindCSS, the app offers a fast and fluid experience.",
+    "collaborations.title": "Featured Collaborations",
 
-        'footer.about': 'This portfolio has been developed with',
+    "form.title": "Don't hesitate to contact me!",
+    "form.name": "Name",
+    "form.email": "Email",
+    "form.subject": "Subject",
+    "form.message": "Say something...",
+    "form.send": "Send",
 
+    "footer.about": "This portfolio has been developed with",
+  },
+  fr: {
+    "tab.title": "Portefeuille de Óscar Rabadán",
 
-    },
-    fr: {
-        'nav.home': 'Présentation',
-        'nav.work': 'Projets en vedette',
-        'nav.collaborations': 'Collaborations',
-        'nav.contact': 'Contact',
+    "nav.home": "Présentation",
+    "nav.work": "Projets en vedette",
+    "nav.collaborations": "Collaborations",
+    "nav.contact": "Contact",
 
-        'hero.desc': "Développeur front-end avec plus de 3 ans d'expérience, j'aime me tenir au courant des dernières tendances du secteur et améliorer continuellement mes connaissances. Je me concentre sur la création d'applications web réactives et performantes, avec un grand souci du détail.",
-        'hero.job': "Développeur front-end",
-        'hero.link': "Envie de découvrir mes projets? N'hésitez pas!",
+    "hero.desc":
+      "Développeur front-end avec plus de 3 ans d'expérience, j'aime me tenir au courant des dernières tendances du secteur et améliorer continuellement mes connaissances. Je me concentre sur la création d'applications web réactives et performantes, avec un grand souci du détail.",
+    "hero.job": "Développeur front-end",
+    "hero.link": "Envie de découvrir mes projets? N'hésitez pas!",
 
-        'skills.languages': 'Langages',
-        'skills.frameworks': 'Frameworks et librairies',
-        'skills.clouds': 'Clouds et Fournisseurs de services',
-        'skills.tools': 'Outils',
+    "skills.languages": "Langages",
+    "skills.frameworks": "Frameworks et librairies",
+    "skills.clouds": "Clouds et Fournisseurs de services",
+    "skills.tools": "Outils",
 
-        'project.tubookbuddy.description': "TuBookBuddy est une plateforme Web conçue pour les amateurs de livres, où les utilisateurs peuvent facilement découvrir, organiser et partager des livres. Propulsée par Next.js et conçue avec TailwindCSS, l'application offre une expérience rapide et fluide.",
-        'collaborations.title': 'Collaborations en vedette',
+    "project.tubookbuddy.description":
+      "TuBookBuddy est une plateforme Web conçue pour les amateurs de livres, où les utilisateurs peuvent facilement découvrir, organiser et partager des livres. Propulsée par Next.js et conçue avec TailwindCSS, l'application offre une expérience rapide et fluide.",
+    "collaborations.title": "Collaborations en vedette",
 
-        'form.title': "N'hésitez pas à me contacter !",
-        'form.name': 'Nom complet',
-        'form.email': 'Email',
-        'form.subject': 'Objet',
-        'form.message': 'Dites quelque chose...',
-        'form.send': 'Envoyer',
+    "form.title": "N'hésitez pas à me contacter !",
+    "form.name": "Nom complet",
+    "form.email": "Email",
+    "form.subject": "Objet",
+    "form.message": "Dites quelque chose...",
+    "form.send": "Envoyer",
 
-        'footer.about': 'Ce portfolio a été développé avec',
+    "footer.about": "Ce portfolio a été développé avec",
+  },
+  es: {
+    "tab.title": "Portfolio de Óscar Rabadán",
 
-    },
-    es: {
-        'nav.home': 'Presentación',
-        'nav.work': 'Proyectos destacados',
-        'nav.collaborations': 'Colaboraciones',
-        'nav.contact': 'Contacto',
+    "nav.home": "Presentación",
+    "nav.work": "Proyectos destacados",
+    "nav.collaborations": "Colaboraciones",
+    "nav.contact": "Contacto",
 
-        'hero.desc': "Frontend Developer con más de 3 años de experiencia. Me gusta mantenerme al día con las últimas tendencias del sector y mejorar continuamente mis conocimientos. Me enfoco en crear aplicaciones web responsivas y de alto rendimiento, prestando especial atención al detalle.",
-        'hero.job': "Frontend Developer",
-        'hero.link': '¿Te gustaría ver algunos de mis proyectos?',
+    "hero.desc":
+      "Frontend Developer con más de 3 años de experiencia. Me gusta mantenerme al día con las últimas tendencias del sector y mejorar continuamente mis conocimientos. Me enfoco en crear aplicaciones web responsivas y de alto rendimiento, prestando especial atención al detalle.",
+    "hero.job": "Frontend Developer",
+    "hero.link": "¿Te gustaría ver algunos de mis proyectos?",
 
-        'project.tubookbuddy.description': 'TuBookBuddy es una plataforma web diseñada para amantes de la lectura, donde los usuarios pueden descubrir, organizar y compartir libros de manera sencilla. Desarrollada con Next.js y estilizada con TailwindCSS, la aplicación ofrece una experiencia rápida y fluida.',
-        'collaborations.title': 'Colaboraciones destacadas',
+    "project.tubookbuddy.description":
+      "TuBookBuddy es una plataforma web diseñada para amantes de la lectura, donde los usuarios pueden descubrir, organizar y compartir libros de manera sencilla. Desarrollada con Next.js y estilizada con TailwindCSS, la aplicación ofrece una experiencia rápida y fluida.",
+    "collaborations.title": "Colaboraciones destacadas",
 
-        'skills.languages': 'Lenguajes',
-        'skills.frameworks': 'Frameworks y librerias',
-        'skills.clouds': 'Clouds y proveedores de servicio',
-        'skills.tools': 'Herramientas',
+    "skills.languages": "Lenguajes",
+    "skills.frameworks": "Frameworks y librerias",
+    "skills.clouds": "Clouds y proveedores de servicio",
+    "skills.tools": "Herramientas",
 
-        'form.title': '¡No dudes en contactarme!',
-        'form.name': 'Nombre completo',
-        'form.email': 'Correo electrónico',
-        'form.subject': 'Asunto',
-        'form.message': 'Escribe aqui tu mensaje...',
-        'form.send': 'Enviar',
+    "form.title": "¡No dudes en contactarme!",
+    "form.name": "Nombre completo",
+    "form.email": "Correo electrónico",
+    "form.subject": "Asunto",
+    "form.message": "Escribe aqui tu mensaje...",
+    "form.send": "Enviar",
 
-        'footer.about': 'Este portafolio ha sido desarrollado con',
-
-
-    },
+    "footer.about": "Este portafolio ha sido desarrollado con",
+  },
 } as const;
 
 export const showDefaultLang = false;
